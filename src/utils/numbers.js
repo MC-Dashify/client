f3480052b56985a3142af9e3ced083efdf652258
ppapman <48434316+ppapman1@users.noreply.github.com>
2023-07-01 01:20:53 +0900

    style: change tab indent to 2 spaces

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -11,12 +11,13 @@ const addCommasToNumbers = (value) => value.toLocaleString();
  * @returns {number}
  */
 const floorDecimal = (number, fractionDigits) => {
-    const multiplier = Math.pow(10, fractionDigits);
-    const flooredNumber = Math.floor(number * multiplier).toString();
-    return (flooredNumber.slice(0, -fractionDigits) + '.' + flooredNumber.slice(-fractionDigits));
-}
-
-export {
-    addCommasToNumbers,
-    floorDecimal
+  const multiplier = Math.pow(10, fractionDigits);
+  const flooredNumber = Math.floor(number * multiplier).toString();
+  return (
+    flooredNumber.slice(0, -fractionDigits) +
+    '.' +
+    flooredNumber.slice(-fractionDigits)
+  );
 };
+
+export { addCommasToNumbers, floorDecimal };
