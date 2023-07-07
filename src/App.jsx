fa754e4db354bc0cadd05e1249d46938dbe43c82
다이츠로 <dytrodev@gmail.com>
2023-07-07 21:07:12 +0900

    test(App): test for recoil state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Root from './routes/Root';
-import ErrorPage from './routes/ErrorPage';
-import DashboardLayout from './routes/DashboardLayout';
-import Overview from './routes/Overview';
-import Stats from './routes/Stats';
-import Worlds from './routes/Worlds';
-import Players from './routes/Players';
-import Traffic from './routes/Traffic';
-import Console from './routes/Console';
-import Settings from './routes/Settings';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Root from "./routes/Root";
+import ErrorPage from "./routes/ErrorPage";
+import DashboardLayout from "./routes/DashboardLayout";
+import Overview from "./routes/Overview";
+import Stats from "./routes/Stats";
+import Worlds from "./routes/Worlds";
+import Players from "./routes/Players";
+import Traffic from "./routes/Traffic";
+import Console from "./routes/Console";
+import Settings from "./routes/Settings";
+
+import { useRecoilState } from "recoil";
+
+import { testState } from "./contexts/states";
+import { useInterval } from "./hooks/interval";
 
 const App = () => {
   const location = useLocation();
   const background = location.state?.background;
 
-  console.log(location, background);
+  const [test, setTest] = useRecoilState(testState);
+
+  // 백그라운드에서 일정 시간마다 작업 실행
+  useInterval(() => {
+    console.log(test)
+    setTest(test + 1)
+  }, 1000)
 
   return (
     <>
