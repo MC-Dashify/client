272aada20d365d8108c549b3cd3cf284debd5e96
다이츠로 <dytrodev@gmail.com>
2023-07-07 22:45:44 +0900

    style(main): change single quotes to double quotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Tooltip as ReactTooltip } from 'react-tooltip';
-import { toast, Toaster } from 'react-hot-toast';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import { toast, Toaster } from "react-hot-toast";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,17 +12,15 @@ import {
   LineElement,
   Tooltip,
   Legend,
-  defaults as chartDefaults
-} from 'chart.js';
-import { registerSW } from 'virtual:pwa-register';
+  defaults as chartDefaults,
+} from "chart.js";
+import { registerSW } from "virtual:pwa-register";
 
-import App from './App';
+import App from "./App";
 
-import AppData from './storage/data';
-import GlobalStyle, {
-  defaultFontFamily
-} from './components/common/globalstyles';
-import './styles/font-settings.css';
+import AppData from "./storage/data";
+import GlobalStyle, { defaultFontFamily } from "./components/common/globalstyles";
+import "./styles/font-settings.css";
 
 ChartJS.register(
   ArcElement,
@@ -36,23 +34,20 @@ ChartJS.register(
 chartDefaults.font.family = defaultFontFamily;
 chartDefaults.font.size = 16;
 
-const VERSION = '0.0.1';
-AppData.set('etc.version', VERSION);
+const VERSION = "0.0.1";
+AppData.set("etc.version", VERSION);
 // XXX VERSION 파일 삭제 논의
 
 const updateSW = registerSW({
   onOfflineReady() {
-    toast(
-      '오프라인 준비를 완료했습니다. 이제 오프라인 상태에서도 애플리케이션을 사용할 수 있습니다.',
-      {
-        id: 'offline-ready',
-        duration: 4000
-      }
-    );
-  }
+    toast("오프라인 준비를 완료했습니다. 이제 오프라인 상태에서도 애플리케이션을 사용할 수 있습니다.", {
+      id: "offline-ready",
+      duration: 4000,
+    });
+  },
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
@@ -62,8 +57,9 @@ root.render(
       <App />
     </BrowserRouter>
 
-    <ReactTooltip id='dashify__tooltip' place='bottom' />
-    <Toaster position='bottom-center' />
+      <ReactTooltip id="dashify__tooltip" place="bottom" />
+      <Toaster position="bottom-center" />
+    </RecoilRoot>
   </React.StrictMode>
 );
 
