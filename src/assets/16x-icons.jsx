f3fb09bbe95ed345e04600e151925a114e81306e
다이츠로 <dytrodev@gmail.com>
2023-07-09 04:46:09 +0900

    feat(Icon): add PlusIcon
    Co-Authored-By: aroxu <30624945+aroxu@users.noreply.github.com>
    Co-Authored-By: ppapman <48434316+ppapman1@users.noreply.github.com>

--- src/assets/16x-icons.jsx
+++ src/assets/16x-icons.jsx
@@ -35,4 +35,11 @@ const XIcon = (props) => (
   </svg>
 );
 
-export { ArrowRightAndLeftIcon, XIcon };
+const PlusIcon = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 16 16" fill="none" {...props}>
+    <path d="M3 8H13" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M8 3L8 13" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+)
+
+export { ArrowRightAndLeftIcon, XIcon, PlusIcon };
