688c3d040873a7ece2c63249415451210d67ef1e
aroxu <30624945+aroxu@users.noreply.github.com>
2023-07-10 00:43:54 +0900

    style: format main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Tooltip as ReactTooltip } from "react-tooltip";
-import { toast, Toaster } from "react-hot-toast";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import { toast, Toaster } from 'react-hot-toast';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,45 +12,58 @@ import {
   LineElement,
   Tooltip,
   Legend,
-  defaults as chartDefaults,
-} from "chart.js";
-import { registerSW } from "virtual:pwa-register";
+  defaults as chartDefaults
+} from 'chart.js';
+import { registerSW } from 'virtual:pwa-register';
 
-import App from "./App";
+import App from './App';
 
-import AppData from "./storage/data";
-import GlobalStyle, { defaultFontFamily } from "./components/common/globalstyles";
-import "./styles/font-settings.css";
+import AppData from './storage/data';
+import GlobalStyle, {
+  defaultFontFamily
+} from './components/common/globalstyles';
+import './styles/font-settings.css';
 
-ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement);
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+);
 chartDefaults.font.family = defaultFontFamily;
 chartDefaults.font.size = 16;
 
-const VERSION = "0.0.1";
-AppData.set("etc.version", VERSION);
+const VERSION = '0.0.1';
+AppData.set('etc.version', VERSION);
 // XXX VERSION 파일 삭제 논의
 
 const updateSW = registerSW({
   onOfflineReady() {
-    toast("오프라인 준비를 완료했습니다. 이제 오프라인 상태에서도 애플리케이션을 사용할 수 있습니다.", {
-      id: "offline-ready",
-      duration: 4000,
-    });
-  },
+    toast.success(
+      '오프라인 준비를 완료했습니다. 이제 오프라인 상태에서도 애플리케이션을 사용할 수 있습니다.',
+      {
+        id: 'offline-ready',
+        duration: 4000
+      }
+    );
+  }
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <GlobalStyle />
+    <GlobalStyle />
 
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 
-      <ReactTooltip id="dashify__tooltip" place="bottom" />
-      <Toaster position="bottom-center" />
+    <ReactTooltip id='dashify__tooltip' place='bottom' />
+    <Toaster position='bottom-center' style={{ zIndex: '20' }} />
   </React.StrictMode>
 );
 
