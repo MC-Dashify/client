47b05c9bdc7582c5a3b3f95cb1324add9e51178a
aroxu <30624945+aroxu@users.noreply.github.com>
2023-07-10 00:42:13 +0900

    feat(utils/net): add jsdoc, add key parameter

--- src/utils/net.js
+++ src/utils/net.js
@@ -1,9 +1,29 @@
 import axios from 'axios';
 
 const Network = {
-  ping: (host, port, isSecureConnection = false) => {
+  /**
+   * @param {string} host
+   * @param {number} port
+   * @param {boolean} isSecureConnection
+   * @returns {Promise}
+   * @example
+   * Network.ping('foo.com', 25565, false)
+   * .then((res) => {
+   *  console.log(res);
+   * })
+   * .catch((err) => {
+   * console.log(err);
+   * });
+   **/
+  ping: (host, port, key, isSecureConnection = false) => {
     return axios.get(
-      `${isSecureConnection ? 'https' : 'http'}://${host}:${port}/ping`
+      // `${isSecureConnection ? 'https' : 'http'}://${host}:${port}/ping`,
+      '/ping',
+      {
+        headers: {
+          Authorization: `Bearer ${key}`
+        }
+      }
     );
   }
 };
