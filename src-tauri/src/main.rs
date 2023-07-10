f9c4e284197beafe2db43eb145713d6cf85469ad
aroxu <30624945+aroxu@users.noreply.github.com>
2023-07-10 17:09:45 +0900

    :sparkle: set default window size

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{LogicalSize, Manager, Size};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+            main_window
+                .set_size(Size::Logical(LogicalSize {
+                    width: 1366.0,
+                    height: 900.0,
+                }))
+                .unwrap();
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
