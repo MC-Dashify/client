ac45b131311780eccd8014f94a4143945e9899fb
aroxu <30624945+aroxu@users.noreply.github.com>
2023-07-12 00:14:24 +0900

    fix: http request

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,45 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{LogicalSize, Manager, Size};
+use tauri::{utils::config::AppUrl, window::WindowBuilder, LogicalSize, Manager, Size, WindowUrl};
 
 fn main() {
+    let port = portpicker::pick_unused_port().expect("failed to find unused port");
+    // let port = 80;
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://dashify.localhost:{}", port)
+        .parse()
+        .unwrap();
+    let window_url = WindowUrl::External(url);
+    // rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+
     tauri::Builder::default()
-        .setup(|app| {
-            let main_window = app.get_window("main").unwrap();
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
+        .setup(move |app| {
+            WindowBuilder::new(
+                app,
+                "Dashify".to_string(),
+                if cfg!(dev) {
+                    Default::default()
+                } else {
+                    window_url
+                },
+            )
+            .title("Dashify")
+            .build()?;
+            let main_window = app.get_window("Dashify").unwrap();
             main_window
                 .set_size(Size::Logical(LogicalSize {
-                    width: 1366.0,
+                    width: 1370.0,
                     height: 900.0,
                 }))
                 .unwrap();
             Ok(())
         })
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
