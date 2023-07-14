de32ee284633c536744a6b8e2f31a570cfd1fefd
aroxu <zeus8502@gmail.com>
2023-07-14 21:34:22 +0900

    style: enhance JSDoc

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -2,6 +2,12 @@
  *
  * @param {string} str format: "100 Bytes", "100 KB", "100 MB", "100 GB", "100 TB" etc
  * @returns {number} bytes
+ * @example
+ * stringToBytes('100 Bytes') // 100
+ * stringToBytes('100 KB') // 102400
+ * stringToBytes('100 MB') // 104857600
+ * stringToBytes('100 GB') // 107374182400
+ * stringToBytes('100 TB') // 109951162777600
  */
 const stringToBytes = (str) => {
   const [number, unit] = str.split(' ');
--- src/utils/modal.js
+++ src/utils/modal.js
@@ -3,6 +3,14 @@ import withReactContent from 'sweetalert2-react-content';
 
 const modalContent = withReactContent(Swal);
 
+/* 
+  @param {string} body - The body of the modal
+  @param {number} width - The width of the modal
+  @param {object} options - The options of the modal
+  @example
+  showModal('Hello World', 600, { title: 'Hello World' })
+  showModal(<div>This is toast</div>, 600, { toast: true })
+*/
 export const showModal = (body, width, options) => {
   modalContent.fire({
     width: width,
