c24f5e7f20781c1acc8437d1a5a730407c2cc3b2
ppapman <48434316+ppapman1@users.noreply.github.com>
2023-08-14 17:16:04 +0900

    feat: remove babel-plugin-styled-components

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      displayName: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
