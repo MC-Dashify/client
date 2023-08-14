71c64d72d75f3ba50059a6e6b38db4eee319219d
ppapman <48434316+ppapman1@users.noreply.github.com>
2023-08-14 17:38:44 +0900

    feat: add LogoSymbol and LogoText, apply @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -2,11 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+
   compiler: {
     styledComponents: {
       ssr: true,
       displayName: true,
     },
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
 
