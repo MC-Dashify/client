b16d85b57e7b4f88d4a6ee39526be7c8ab262adb
ppapman <48434316+ppapman1@users.noreply.github.com>
2023-12-22 21:35:58 +0900

    feat: apply theme based on local storage
    w/o nextui

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import LocalStorage from "@/lib/localstorage";
 
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(initialValue);
   const [firstLoadDone, setFirstLoadDone] = useState(false);
+
+  const setItem = useCallback(
+    (value) => {
+      if (typeof window !== "undefined") {
+        LocalStorage.setItem(key, value);
+        window.dispatchEvent(
+          new CustomEvent("storage", { detail: { key, newValue: value } })
+        );
+        // FIXME 로컬 스토리지 변화가 다른 탭에서는 감지되지 않습니다.
+        // 첨언: 프로젝트가 사용하는 tauri 버전에서는 여러 프로세스가 열려 있을 경우 서로 스토리지가
+        // 충돌하는 문제가 있습니다. https://github.com/tauri-apps/wry/issues/621
+      }
+    },
+    [key]
+  );
 
   useEffect(() => {
     const fromLocal = () => {
@@ -32,13 +47,26 @@ export const useLocalStorage = (key, initialValue) => {
     }
 
     try {
-      if (typeof window !== "undefined") {
-        LocalStorage.setItem(key, storedValue);
-      }
+      setItem(storedValue);
     } catch (error) {
       console.error(error);
     }
-  }, [storedValue, firstLoadDone, key]);
+  }, [storedValue, firstLoadDone, key, setItem]);
+
+  useEffect(() => {
+    const handleStorageChange = (e) => {
+      if (e.detail && e.detail.key === key) {
+        setStoredValue(e.detail.newValue);
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    window.dispatchEvent(new Event("storage"));
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, [key]);
 
   return [storedValue, setStoredValue];
 };
@@ -62,6 +90,7 @@ export const useLaungage = () => {
 };
 
 export const useTheme = () => {
+  // light / dark / system
   const [theme, setTheme] = useLocalStorage(
     "settings.appearance.theme",
     "system" // XXX dropdown options를 contants 폴더로 옮겨서 여기로 import하면 key를 쓸 수 있지 않을까
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
-import { NextUIProvider } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 import { JetBrains_Mono } from "next/font/google";
 import localFont from "next/font/local";
+import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider } from "styled-components";
 
 import "@/styles/dist/tailwindOutput.css";
 import GlobalStyle from "@/components/styles/GlobalStyle";
 import { dark as darkTheme, light as lightTheme } from "@/styles/themes";
+import { useTheme } from "@/hooks/useLocalStorage";
 
 const pretendardFont = localFont({
   src: "../styles/font/PretendardVariable.ttf",
@@ -25,24 +27,21 @@ const jetBrainsMonoFont = JetBrains_Mono({
 });
 
 export default function RootLayout({ Component, pageProps, router }) {
+  const [selectedTheme, setSelectedTheme] = useTheme();
+  const [theme, setTheme] = useState("light");
+
   /** getLayout은 페이지에서 반환되는 함수입니다(`./dashboard/overview.js` 등 참고).
-   * getLayout으로 반환되는 내용은 페이지를 전환해도 바뀌지 않기 하기 위해 사용합니다.
-   */
+   * getLayout으로 반환되는 내용은 페이지를 전환해도 바뀌지 않습니다. */
   const getLayout = Component.getLayout || ((page) => page);
 
-  const selectedTheme = "white"; // TODO 테마 선택 기능 추가
-  let theme;
+  useEffect(() => {
+    const isDarkThemeUsed =
+      selectedTheme === "dark" ||
+      (selectedTheme === "system" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
 
-  switch (selectedTheme) {
-    case "dark":
-      theme = darkTheme;
-      break;
-
-    case "light":
-    default:
-      theme = lightTheme;
-      break;
-  }
+    setTheme(isDarkThemeUsed ? "dark" : "light");
+  }, [selectedTheme]);
 
   return (
     <NextUIProvider>
@@ -53,7 +52,7 @@ export default function RootLayout({ Component, pageProps, router }) {
         }
       `}</style>
 
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
         <GlobalStyle />
 
         {getLayout(<Component {...pageProps} key={router.asPath} />)}
